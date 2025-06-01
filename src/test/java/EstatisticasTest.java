382df1d35b5278a47274c6383a56684ecdb2aff1
202404407 <up202404407@up.pt>
2025-06-01 12:56:26 +0100

    EstatistcasTest

--- src/test/java/EstatisticasTest.java
+++ src/test/java/EstatisticasTest.java
@@ -1,4 +1,5 @@
 import org.example.model.FrequenciaCardiacaArray;
+import org.example.model.TecnicoDeSaude;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.example.model.Data;
@@ -10,15 +11,12 @@ public class EstatisticasTest {
 
     @Test
     void testCalcularMaximo_ComMedicoes() {
-        Data dataInicial = new Data("2025/5/1");
-        Data dataFinal = new Data("2025/5/31");
+        Data dataInicial = new Data("2025/05/01");
+        Data dataFinal = new Data("2025/05/31");
 
-        ArrayList<Integer> medicao = new ArrayList<>();
-        medicao.add(85);
-        medicao.add(90);
-        medicao.add(120);
-
-        fcArray.setMedicao(medicao);
+        fcArray.adicionarMedicao(85, "2025/5/10", new TecnicoDeSaude("José", "1980/07/05", "Enfermeiro"));
+        fcArray.adicionarMedicao(90, "2025/5/15", new TecnicoDeSaude("Bruno", "2000/03/29", "Médico"));
+        fcArray.adicionarMedicao(120, "2025/5/20", new TecnicoDeSaude("Ana", "1985/02/17", "Enfermeira"));
 
         assertEquals(120, fcArray.calcularMaximo(dataInicial, dataFinal));
     }
@@ -36,12 +34,9 @@ public class EstatisticasTest {
         Data dataInicial = new Data("2025/5/1");
         Data dataFinal = new Data("2025/5/31");
 
-        ArrayList<Integer> medicao = new ArrayList<>();
-        medicao.add(85);
-        medicao.add(90);
-        medicao.add(120);
-
-        fcArray.setMedicao(medicao);
+        fcArray.adicionarMedicao(85, "2025/5/10", new TecnicoDeSaude("José", "1980/07/05", "Enfermeiro"));
+        fcArray.adicionarMedicao(90, "2025/5/15", new TecnicoDeSaude("Bruno", "2000/03/29", "Médico"));
+        fcArray.adicionarMedicao(120, "2025/5/20", new TecnicoDeSaude("Ana", "1985/02/17", "Enfermeira"));
 
         assertEquals(85, fcArray.calcularMinimo(dataInicial, dataFinal));
     }
@@ -59,19 +54,15 @@ public class EstatisticasTest {
             Data dataInicial = new Data("2025/5/1");
             Data dataFinal = new Data("2025/5/31");
 
-            ArrayList<Integer> medicao = new ArrayList<>();
-            medicao.add(85);
-            medicao.add(90);
-            medicao.add(120);
-
-            fcArray.setMedicao(medicao);
+            fcArray.adicionarMedicao(85, "2025/5/10", new TecnicoDeSaude("José", "1980/07/05", "Enfermeiro"));
+            fcArray.adicionarMedicao(90, "2025/5/15", new TecnicoDeSaude("Bruno", "2000/03/29", "Médico"));
+            fcArray.adicionarMedicao(120, "2025/5/20", new TecnicoDeSaude("Ana", "1985/02/17", "Enfermeira"));
 
             assertEquals(98.33, fcArray.calcularMedia(dataInicial, dataFinal), 0.01);
         }
 
         @Test
         void testCalcularMedia_SemMedicoes() {
-            ArrayList<Integer> medicao = new ArrayList<>();
             Data dataInicial = new Data("2025/5/1");
             Data dataFinal = new Data("2025/5/31");
 
@@ -83,14 +74,11 @@ public class EstatisticasTest {
             Data dataInicial = new Data("2025/5/1");
             Data dataFinal = new Data("2025/5/31");
 
-            ArrayList<Integer> medicao = new ArrayList<>();
-            medicao.add(85);
-            medicao.add(90);
-            medicao.add(120);
+            fcArray.adicionarMedicao(85, "2025/5/10", new TecnicoDeSaude("José", "1980/07/05", "Enfermeiro"));
+            fcArray.adicionarMedicao(90, "2025/5/15", new TecnicoDeSaude("Bruno", "2000/03/29", "Médico"));
+            fcArray.adicionarMedicao(120, "2025/5/20", new TecnicoDeSaude("Ana", "1985/02/17", "Enfermeira"));
 
-            fcArray.setMedicao(medicao);
-
-            assertEquals(15.275, fcArray.calcularDesvioPadrao(dataInicial, dataFinal), 0.001);
+            assertEquals(15.456, fcArray.calcularDesvioPadrao(dataInicial, dataFinal), 0.001);
         }
 
         @Test
